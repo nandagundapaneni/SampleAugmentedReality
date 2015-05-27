9be558fdc0ab6f6aae21debb06f48301a7fc4ffd
Nanda Gundapaneni <nanda.gundapaneni@gmail.com>
2015-05-27 14:08:15 -0600

    UI

--- AugmentViewController.m
+++ AugmentViewController.m
@@ -44,6 +44,8 @@ static const double Radius = 3200;
             CGAffineTransform scale = CGAffineTransformScale(translate, 1.333333, 1.333333);
             self.cameraViewTransform = scale;
             
+            [SVProgressHUD setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.7]];
+            [SVProgressHUD setForegroundColor:[UIColor whiteColor]];
         }
         
         
@@ -113,6 +115,7 @@ static const double Radius = 3200;
         
         self.currentLocation = newLocation;
         
+
         [SVProgressHUD show];
         [[PlacesDataController manager] retrievePlacesOfInterestForLocation:currentLocation inRadius:Radius onCompletion:^(Places *placesData, NSError *error) {
             [SVProgressHUD dismiss];
